d8696ea8e62eb6a7966269943c882227221906b8
namestesensei <christopherwright435@gmail.com>
2024-07-24 04:07:02 +0000

    Initial commit of version 1.1: Added README.md, AUTHORS, man_1_simple_shell, main.h, shell.c, betty_checks.sh

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>   /* This lets us use functions like malloc and free */
 #include <unistd.h>   /* This lets us use system calls like execve */
 #include <string.h>   /* This lets us use functions like strtok to split strings */
+#include <sys/types.h>/* This lets us use various data types */
 #include <sys/wait.h> /* This lets us use the wait function to wait for child processes */
 
 void prompt(void);
--- shell.c
+++ shell.c
@@ -7,46 +7,46 @@
  */
 int main(void)
 {
-    char *line = NULL;   /* This will store the command we type */
-    size_t len = 0;      /* This will store the size of the buffer */
-    ssize_t nread;       /* This will store the number of characters read by getline */
-    char *argv[10];      /* This will store the command and its arguments */
-    int argc;            /* This will store the number of arguments */
+	char *line = NULL;   /* This will store the command we type */
+	size_t len = 0;      /* This will store the size of the buffer */
+	ssize_t nread;       /* This will store the number of characters read by getline */
+	char *argv[10];      /* This will store the command and its arguments */
+	int argc;            /* This will store the number of arguments */
 
-    while (1)  /* This loop will run forever until we tell it to stop */
-    {
-        prompt();   /* Print the prompt to the screen */
+	while (1)  /* This loop will run forever until we tell it to stop */
+	{
+		prompt();   /* Print the prompt to the screen */
 
-        nread = getline(&line, &len, stdin);   /* Read the command from the user */
+		nread = getline(&line, &len, stdin);   /* Read the command from the user */
 
-        if (nread == -1)   /* If we get an error or EOF (Ctrl+D), we exit the loop */
-        {
-            free(line);   /* Free the memory we allocated for the command */
-            exit(EXIT_SUCCESS);   /* Exit the program successfully */
-        }
+		if (nread == -1)   /* If we get an error or EOF (Ctrl+D), we exit the loop */
+		{
+			free(line);   /* Free the memory we allocated for the command */
+			exit(EXIT_SUCCESS);   /* Exit the program successfully */
+		}
 
-        line[nread - 1] = '\0';   /* Remove the newline character at the end */
+		line[nread - 1] = '\0';   /* Remove the newline character at the end */
 
-        argc = 0;   /* Initialize the argument count */
-        argv[argc] = strtok(line, " ");   /* Split the command into words */
+		argc = 0;   /* Initialize the argument count */
+		argv[argc] = strtok(line, " ");   /* Split the command into words */
 
-        while (argv[argc] != NULL)   /* Split the rest of the command */
-            argv[++argc] = strtok(NULL, " ");
+		while (argv[argc] != NULL)   /* Split the rest of the command */
+			argv[++argc] = strtok(NULL, " ");
 
-        if (fork() == 0)   /* Create a child process */
-        {
-            execve(argv[0], argv, NULL);   /* Execute the command in the child process */
-            perror("execve");   /* If execve fails, print an error message */
-            exit(EXIT_FAILURE);   /* Exit the child process if execve fails */
-        }
-        else   /* Parent process waits for the child to finish */
-        {
-            wait(NULL);   /* Wait for the child process to finish */
-        }
-    }
+		if (fork() == 0)   /* Create a child process */
+		{
+			execve(argv[0], argv, NULL);   /* Execute the command in the child process */
+			perror("execve");   /* If execve fails, print an error message */
+			exit(EXIT_FAILURE);   /* Exit the child process if execve fails */
+		}
+		else   /* Parent process waits for the child to finish */
+		{
+			wait(NULL);   /* Wait for the child process to finish */
+		}
+	}
 
-    free(line);   /* Free the memory we allocated for the command */
-    return 0;     /* Return 0 to indicate the program ended successfully */
+	free(line);   /* Free the memory we allocated for the command */
+	return (0);     /* Return 0 to indicate the program ended successfully */
 }
 
 /**
@@ -54,6 +54,6 @@ int main(void)
  */
 void prompt(void)
 {
-    printf("#cisfun$ ");   /* Print the prompt to the screen */
+	printf("#cisfun$ ");   /* Print the prompt to the screen */
 }
 
