6c63024256122a0fb4774b4e7cace8f9e3eb6b2b
namestesensei <christopherwright435@gmail.com>
2024-08-02 05:44:03 +0000

    Implement simple shell 0.1 with Betty style and documentation

--- main.h
+++ main.h
@@ -1,9 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Author: Your Name
+ * Description: Header file containing function prototypes for simple shell.
+ */
+
 extern char **environ;
 
+/**
+ * handle_command - Handles execution of a command.
+ * @args: Array of arguments.
+ */
 void handle_command(char *args[]);
+
+/**
+ * find_command - Finds the full path of a command.
+ * @command: The command to find.
+ *
+ * Return: The full path of the command, or NULL if not found.
+ */
 char *find_command(char *command);
 
 #endif /* MAIN_H */
